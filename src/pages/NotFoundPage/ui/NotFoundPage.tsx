d58bbf91285dcb265e90642ab9d77c8c35fb4d2f
Timmy2324 <Salikhov.T.S@yandex.ru>
2023-08-13 23:45:21 +0400

    Added not found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+import { useTranslation } from 'react-i18next';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './NotFoundPage.module.scss';
+
+export const NotFoundPage = () => {
+    const { t } = useTranslation('notFound');
+
+    return (
+        <div className={classNames(cls.notFound, {}, [])}>
+            {t('Page not found')}
+        </div>
+    );
+};
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { RouteProps } from 'react-router-dom';
 import { AboutPage } from '@/pages/AboutPage';
 import { MainPage } from '@/pages/MainPage';
+import { NotFoundPage } from '@/pages/NotFoundPage';
 
 export enum AppRoutes {
     Main = 'main',
     About = 'about',
+    NotFound = 'notFound',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
     [AppRoutes.Main]: '/',
     [AppRoutes.About]: '/about',
+    [AppRoutes.NotFound]: '*',
 };
 
 export const routeConfig: RouteProps[] = [
@@ -20,5 +23,9 @@ export const routeConfig: RouteProps[] = [
     {
         path: RoutePath.about,
         element: <AboutPage />,
+    },
+    {
+        path: RoutePath.notFound,
+        element: <NotFoundPage />,
     },
 ];
